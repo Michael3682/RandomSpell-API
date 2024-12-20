bef2c83959f96d8aac175b2c29c8f20123d611af
Michael3682 <dominiquedelacerna5@gmail.com>
2024-12-20 23:52:36 +0800

    changes added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-   navigator.serviceWorker.register('../service-worker.js').then( () => {
+   navigator.serviceWorker.register('/sw.js').then( () => {
     console.log('Service Worker Registered')
    })
  })
